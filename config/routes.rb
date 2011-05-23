decedf5f3db562a8c641cede6d08b04ebd8025a2
chris <porios@yahoo.gr>
2011-05-23 12:44:04 +0300

    Basic user model finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Twitar::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
@@ -7,6 +7,7 @@ Twitar::Application.routes.draw do
 
   match '/signup', :to => 'users#new'
 
+ 
   root :to => 'pages#home'
 
   # The priority is based upon order of creation:
